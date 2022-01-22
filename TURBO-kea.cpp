9435923a0592a5929ff6fabb55722e81d31b4367
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2022-01-22 11:52:44 +0100

    Update TURBO-kea.cpp

--- TURBO-kea.cpp
+++ TURBO-kea.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
 using namespace std;
 
 int var_liste[512] = { 0 };
 string var_name[512] = { "unset" };
 int maxset = 0;
+bool DEBUGPRINT = true;
 
 void setvar(string name, int vall) {
 	for (int i = 0; i <= maxset; i++) {
@@ -12,7 +16,6 @@ void setvar(string name, int vall) {
 			return;
 		}
 	}
-
 	maxset++;
 	var_liste[maxset] = vall;
 	var_name[maxset] = name;
@@ -28,7 +31,39 @@ int getvar(string name) {
 	return 0;
 }
 
+vector<string> split(string x, char delim = ' ')
+{
+	x += delim; //includes a delimiter at the end so last word is also read
+	vector<string> splitted;
+	string temp = "";
+	for (int i = 0; i < x.length(); i++)
+	{
+		if (x[i] == delim)
+		{
+			splitted.push_back(temp); //store words in "splitted" vector
+			temp = "";
+			i++;
+		}
+		temp += x[i];
+	}
+	return splitted;
+}
+
+
+void codeinloop(vector<string>, string nom, int max) {
+	if (DEBUGPRINT) {
+		cout << "demarage de la boucle: " << nom << endl;
+	}
+	string sauter = nom;
+}
+
+void start(string code) {
+	vector<string> sortie = split(code, ';');
+	codeinloop(sortie, "main", 1);
+}
+
+
 int main() {
-	setvar("coucou", 42);
-	cout << "coucou = " << getvar("coucou") << endl;
+	start("V 1 1;V 2 2");
+	return EXIT_SUCCESS;
 }
